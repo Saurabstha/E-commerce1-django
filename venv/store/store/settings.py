cf7037319f6383c1dbf4a365755e75485b2c0b39
unknown <saurab.shrestha99@gmail.com>
2020-01-13 12:11:59 +0545

    changed settings.py

--- venv/store/store/settings.py
+++ venv/store/store/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com' # mail service smtp
 EMAIL_HOST_USER = 'saurab.shrestha99@gmail.com' # email id
-EMAIL_HOST_PASSWORD = '9842582178gmail' #password
+EMAIL_HOST_PASSWORD = '' #password
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
